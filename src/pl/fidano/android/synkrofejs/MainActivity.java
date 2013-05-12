68fd3aeb7f82827fdd443608bd796c43dd92287d
Mariusz Fik <fisiu@opensuse.org>
2013-05-12 19:33:10 +0200

    Initial commit.
    Signed-off-by: Mariusz Fik <fisiu@opensuse.org>

---
+++ src/pl/fidano/android/synkrofejs/MainActivity.java
@@ -0,0 +1,26 @@
+package pl.fidano.android.synkrofejs;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		TextView label = (TextView) findViewById(R.id.firstLabel);
+		label.setText("Text set during runtime.");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
