78145e1d9a008d0246300945618076a93bf37a6f
Mariusz Fik <fisiu@opensuse.org>
2013-05-13 20:00:06 +0200

    Fetch contacts and display them in the list.
    Signed-off-by: Mariusz Fik <fisiu@opensuse.org>

--- src/pl/fidano/android/synkrofejs/MainActivity.java
+++ src/pl/fidano/android/synkrofejs/MainActivity.java
@@ -1,26 +1,44 @@
 package pl.fidano.android.synkrofejs;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_main);
 
-		TextView label = (TextView) findViewById(R.id.firstLabel);
-		label.setText("Text set during runtime.");
-	}
+	Log.i(getClass().getSimpleName(), "onCreate()");
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+	TextView label = (TextView) findViewById(R.id.firstLabel);
+	label.setText("Text set during runtime :)");
+
+	ListView lv = (ListView) findViewById(R.id.contactsList);
+
+	Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null,
+		null, null);
+	ListAdapter adapter = new SimpleCursorAdapter(this, R.layout.contact_item, cursor, new String[] {
+		ContactsContract.Contacts.DISPLAY_NAME, ContactsContract.CommonDataKinds.Email.DISPLAY_NAME,
+		ContactsContract.Contacts._ID }, new int[] { R.id.contactName, R.id.contactDetails, R.id.contactID }, 0);
+	lv.setAdapter(adapter);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+	// Inflate the menu; this adds items to the action bar if it is present.
+	getMenuInflater().inflate(R.menu.main, menu);
+	return true;
+    }
 
 }
